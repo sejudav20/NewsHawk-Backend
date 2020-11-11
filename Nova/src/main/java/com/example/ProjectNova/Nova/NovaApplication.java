dd21dfb554bac4122d07a9564354d3620f8813e7
Davin Seju <davsej100@gmail.com>
2020-11-10 23:23:26 -0800

    Initial Creation of Spring API

---
+++ Nova/src/main/java/com/example/ProjectNova/Nova/NovaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjectNova.Nova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovaApplication.class, args);
+	}
+
+}
---
+++ Nova/src/test/java/com/example/ProjectNova/Nova/NovaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjectNova.Nova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
