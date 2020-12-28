c75335b8c15c5fa11d39ccddde4b1f947373183f
Davin Seju <davsej100@gmail.com>
2020-12-27 20:25:27 -0800

    added table creation methods

--- Nova/src/main/java/com/example/ProjectNova/Nova/DAO/AWSUserDAO.java
+++ Nova/src/main/java/com/example/ProjectNova/Nova/DAO/AWSUserDAO.java
@@ -4,12 +4,136 @@ import com.example.ProjectNova.Nova.Model.Article;
 import com.example.ProjectNova.Nova.Model.Comment;
 import com.example.ProjectNova.Nova.Model.User;
 import com.example.ProjectNova.Nova.Model.UserContent;
+import com.example.ProjectNova.Nova.Service.AWSInitializer;
 import org.springframework.stereotype.Repository;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.*;
 
 import java.util.List;
 
 @Repository("UserDao")
 public class AWSUserDAO implements UserDAO{
+    public DynamoDbClient getDynamo() {
+        return new AWSInitializer().getClient();
+    }
+
+    public void createReadlistTable () {
+        DynamoDbClient client = getDynamo();
+
+
+        ///trying to create readlist table
+        CreateTableRequest ctr = CreateTableRequest.builder().attributeDefinitions(
+                AttributeDefinition.builder().attributeName("name").attributeType(ScalarAttributeType.S).build(),
+                AttributeDefinition.builder().attributeName("author").attributeType(ScalarAttributeType.S).build())
+                .keySchema(KeySchemaElement.builder()
+                                .attributeName("name")
+                                .keyType(KeyType.HASH)
+                                .build(),
+                        KeySchemaElement.builder().attributeName("author").keyType(KeyType.RANGE).build()).provisionedThroughput(ProvisionedThroughput.builder()
+                        .readCapacityUnits(10L)
+                        .writeCapacityUnits(10L)
+                        .build()).tableName("ReadLists").build();
+        try {
+            CreateTableResponse response = client.createTable(ctr);
+        } catch (DynamoDbException s) {
+
+        }
+
+
+    }
+    public void createUserTable () {
+        DynamoDbClient client = getDynamo();
+
+
+        ///trying to create user table table
+        CreateTableRequest ctr = CreateTableRequest.builder().attributeDefinitions(
+                AttributeDefinition.builder().attributeName("name").attributeType(ScalarAttributeType.S).build()
+        )
+                .keySchema(KeySchemaElement.builder()
+                        .attributeName("name")
+                        .keyType(KeyType.HASH)
+                        .build()).provisionedThroughput(ProvisionedThroughput.builder()
+                        .readCapacityUnits(10L)
+                        .writeCapacityUnits(10L)
+                        .build()).tableName("Users").build();
+        try {
+            CreateTableResponse response = client.createTable(ctr);
+        } catch (DynamoDbException s) {
+
+        }
+
+
+    }
+    public void createUserContentTable () {
+        DynamoDbClient client = getDynamo();
+
+
+        ///trying to create user table table
+        CreateTableRequest ctr = CreateTableRequest.builder().attributeDefinitions(
+                AttributeDefinition.builder().attributeName("name").attributeType(ScalarAttributeType.S).build()
+        )
+                .keySchema(KeySchemaElement.builder()
+                        .attributeName("name")
+                        .keyType(KeyType.HASH)
+                        .build()).provisionedThroughput(ProvisionedThroughput.builder()
+                        .readCapacityUnits(10L)
+                        .writeCapacityUnits(10L)
+                        .build()).tableName("UserContent").build();
+        try {
+            CreateTableResponse response = client.createTable(ctr);
+        } catch (DynamoDbException s) {
+
+        }
+
+    }
+    public void createCommentsTable () {
+        DynamoDbClient client = getDynamo();
+
+
+        ///trying to create user table table
+        CreateTableRequest ctr = CreateTableRequest.builder().attributeDefinitions(
+                AttributeDefinition.builder().attributeName("id").attributeType(ScalarAttributeType.S).build(),
+                AttributeDefinition.builder().attributeName("username+timestamp").attributeType(ScalarAttributeType.S).build()
+        )
+                .keySchema(KeySchemaElement.builder()
+                        .attributeName("id")
+                        .keyType(KeyType.HASH)
+                        .build(),KeySchemaElement.builder()
+                        .attributeName("username+timestamp")
+                        .keyType(KeyType.RANGE)
+                        .build()).provisionedThroughput(ProvisionedThroughput.builder()
+                        .readCapacityUnits(10L)
+                        .writeCapacityUnits(10L)
+                        .build()).tableName("Comments").build();
+        try {
+            CreateTableResponse response = client.createTable(ctr);
+        } catch (DynamoDbException s) {
+
+        }
+
+
+    }
+
+    public void createArticleTable() {
+        DynamoDbClient client = getDynamo();
+        CreateTableRequest ctr2 = CreateTableRequest.builder().attributeDefinitions(
+                AttributeDefinition.builder().attributeName("title").attributeType(ScalarAttributeType.S).build(),
+                AttributeDefinition.builder().attributeName("author").attributeType(ScalarAttributeType.S).build())
+                .keySchema(KeySchemaElement.builder()
+                                .attributeName("title")
+                                .keyType(KeyType.HASH)
+                                .build(),
+                        KeySchemaElement.builder().attributeName("author").keyType(KeyType.RANGE).build()).provisionedThroughput(ProvisionedThroughput.builder()
+                        .readCapacityUnits(10L)
+                        .writeCapacityUnits(10L)
+                        .build()).tableName("Articles").build();
+        try {
+            CreateTableResponse response = client.createTable(ctr2);
+        } catch (DynamoDbException s) {
+
+        }
+    }
+
     @Override
     public User createUser(User user) {
         return null;
