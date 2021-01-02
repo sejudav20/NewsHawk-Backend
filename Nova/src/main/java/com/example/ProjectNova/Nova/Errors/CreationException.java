0aac1ce8243b7417cb626a1f4e69310ed9c2dddc
Peiyu Lu <Lupeiyu1112@gmail.com>
2021-01-02 12:19:02 -0800

    updated the AWSUserDAO

---
+++ Nova/src/main/java/com/example/ProjectNova/Nova/Errors/CreationException.java
@@ -0,0 +1,8 @@
+package com.example.ProjectNova.Nova.Errors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.CONFLICT, reason = "The object in creation could not be stored")
+public class CreationException extends Exception{
+}
